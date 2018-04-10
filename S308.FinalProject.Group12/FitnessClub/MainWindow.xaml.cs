7137b04e163bb8e01f5789502d25f71d3e227b37
zdobos <zdobos@indiana.edu>
2018-04-09 23:15:06 -0400

    Attempted to fix the Main Window

--- S308.FinalProject.Group12/FitnessClub/MainWindow.xaml.cs
+++ S308.FinalProject.Group12/FitnessClub/MainWindow.xaml.cs
@@ -10,25 +10,20 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace FitnessClub
 {
     /// <summary>
-    /// Interaction logic for Window1.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class MainWindow : Window
     {
-        public Window1()
+        public MainWindow()
         {
-
-
-
-            // HELLO
-
-
             InitializeComponent();
         }
-
     }
 }
+
