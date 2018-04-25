589e0110349bfec3dcbc93ad2739edb51d79a403
zdobos <zdobos@indiana.edu>
2018-04-25 15:24:20 -0400

    m

--- S308.FinalProject.Group12/FitnessClub/MembershipSales.xaml.cs
+++ S308.FinalProject.Group12/FitnessClub/MembershipSales.xaml.cs
@@ -99,5 +99,76 @@ namespace FitnessClub
             //Return ture (as status) to the calling code
             return true;
         }
+
+        private void btnSubmitMembership_Click(object sender, RoutedEventArgs e)
+        {
+            string strFilePath = @"..\..\..\Data\Customers.json";
+            long phone;
+
+            //validate the input FirstName(input)
+            if (txtFirstNameData.Text.Trim() == "")
+            {
+                MessageBox.Show("First Name must be provided.");
+                return;
+            }
+
+            //validate the input LastName(input)
+            if (txtLastNameData.Text.Trim() == "")
+            {
+                MessageBox.Show("Last Name must be provided.");
+                return;
+            }
+
+            //validate the input Phone (input provided, 10 digits within properties, number)
+            if (txtPhoneData.Text.Trim() == "")
+            {
+                MessageBox.Show("Phone must be provided.");
+                return;
+            }
+
+            if (!Int64.TryParse(txtPhoneData.Text.Trim(), out phone))
+            {
+                MessageBox.Show("Phone must be a number.");
+                return;
+            }
+
+
+            //Establish Phone as a number so that I an made sure that the value fall within 10 digits this is for validation establish as a string, then convert to a long then establish connection with string and txtphone then convert string into long and then ensure that it is within the right range else user gets a message
+            string strPhone;
+            long lngPhone;
+            strPhone = txtPhoneData.Text.Trim();
+            lngPhone = Convert.ToInt64(strPhone);
+            if (lngPhone < 1000000000 || lngPhone > 9999999999)
+            {
+                MessageBox.Show("Phone must be 10 digits.");
+                return;
+            }
+
+            //Declare Customers class
+            Customers customerNew = new Customers(txtFirstNameData.Text.Trim(), txtLastNameData.Text.Trim(), txtWeightData.Text.Trim(), txtGenderData.Text.Trim(), txtPhoneData.Text.Trim(), txtEmailData.Text.Trim(), txtAgeData.Text.Trim(), cbxMembershipTypeData.Text, txtStartDateData.Text.Trim(), txtEndDateData.Text.Trim(), txtTrainingPlanData.Text.Trim(), txtLockerRentalData.Text.Trim(), cbxCreditCardTypeData.Text, txtCreditCardNumberData.Text.Trim());
+
+            //instantiate a new Campus from the input and add it to the list
+            customerList.Add(customerNew);
+
+
+            try
+            {
+                //serialize the new list of campuses to json format
+                string jsonData = JsonConvert.SerializeObject(customerList);
+
+                //use System.IO.File to write over the file with the json data
+                System.IO.File.WriteAllText(strFilePath, jsonData);
+
+                MessageBox.Show(customerList.Count + " Cusomters have been exported.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in export process: " + ex.Message);
+            }
+
+            MessageBox.Show("Campus Added!" + Environment.NewLine + customerNew.ToString());
+
+
+        }
     }
 }
