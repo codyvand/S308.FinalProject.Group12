ec3ddb95d119f4513478840f822a80f3256ff248
zdobos <zdobos@indiana.edu>
2018-04-25 08:52:44 -0400

    Added the code for MemberInformation

--- S308.FinalProject.Group12/FitnessClub/MemberInformation.xaml.cs
+++ S308.FinalProject.Group12/FitnessClub/MemberInformation.xaml.cs
@@ -19,9 +19,42 @@ namespace FitnessClub
     /// </summary>
     public partial class MemberInformation : Window
     {
+        List<Customers> customerList;
         public MemberInformation()
         {
             InitializeComponent();
+            //instantiate a list to hold the Campuses
+            customerList = new List<Customers>();
+            dtgFindResults.ItemsSource = customerList;
+
+            //call the method to local the campus information and display
+            ImportCustomerData();
+              private void ImportCustomerData()
+        {
+            string strFilePath = @"..\..\..\Data\Customers.json";
+
+            try
+            {
+                //use System.IO.File to read the entire data file
+                string jsonData = File.ReadAllText(strFilePath);
+
+                //serialize the json data to a list of campuses
+                customerList = JsonConvert.DeserializeObject<List<Customers>>(jsonData);
+
+                if (customerList.Count >= 0)
+                    MessageBox.Show(customerList.Count + " Campuses have been imported.");
+                else
+                    MessageBox.Show("No Campuses has been imported. Please check your file.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in import process: " + ex.Message);
+            }
+
+            //set the source of the datagrid and refresh
+            dtgFindResults.ItemsSource = customerList;
+            dtgFindResults.Items.Refresh();
         }
+    }
     }
 }
