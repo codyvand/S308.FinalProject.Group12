b97dd6894538024de07999da892827283af6b6a8
zdobos <zdobos@indiana.edu>
2018-04-26 21:09:33 -0400

    Updated Features to include the right stuff

--- S308.FinalProject.Group12/FitnessClub/Features.cs
+++ S308.FinalProject.Group12/FitnessClub/Features.cs
@@ -25,18 +25,24 @@ namespace FitnessClub
         {
 
         }
-        public Features(string individualsinglemonth, string individualtwelvemonth, string twosinglemonth, string twotwelvemonth, string familysinglemonth, string familytwelvemonth)
+        public Features(string individualsinglemonth, string individualsinglemonthcheck, string individualtwelvemonth, string individualtwelvemonthcheck, string twosinglemonth, string twosinglemonthcheck, string twotwelvemonth, string twotwelvemonthcheck, string familysinglemonth, string familysinglemonthcheck, string familytwelvemonth, string familytwelvemonthcheck)
         {
             IndividualSingleMonth = individualsinglemonth;
+            IndividualSingleMonthCheck = individualsinglemonthcheck;
             IndividualTwelveMonth = individualtwelvemonth;
+            IndividualTwelveMonthCheck = individualtwelvemonthcheck;
             TwoSingleMonth = twosinglemonth;
+            TwoSingleMonthCheck = twosinglemonthcheck;
             TwoTwelveMonth = twotwelvemonth;
+            TwoTwelveMonthCheck = twotwelvemonthcheck;
             FamilySingleMonth = familysinglemonth;
+            FamilySingleMonthCheck = familysinglemonthcheck;
             FamilyTwelveMonth = familytwelvemonth;
+            FamilyTwelveMonthCheck = familytwelvemonthcheck;
         }
         public override string ToString()
         {
-            return "Features" + Environment.NewLine + "Individual Single Month:" + IndividualSingleMonth + Environment.NewLine + "Individual Twelve Month:" + IndividualTwelveMonth + Environment.NewLine + "Two Single Month:" + TwoSingleMonth + Environment.NewLine + "Two Twelve Month:" + TwoTwelveMonth + Environment.NewLine + "Family Single Month:" + FamilySingleMonth + Environment.NewLine + "Family Twelve Month:" + FamilyTwelveMonth;
+            return "Features" + Environment.NewLine + "Individual Single Month:" + IndividualSingleMonth + Environment.NewLine + "Individual Single Month Check:" + IndividualSingleMonthCheck + Environment.NewLine + "Individual Twelve Month:" + IndividualTwelveMonth + Environment.NewLine +"Individual Twelve Month Check:" + IndividualTwelveMonthCheck + Environment.NewLine + "Two Single Month:" + TwoSingleMonth + Environment.NewLine + "Two Single Month Check:" + TwoSingleMonthCheck + Environment.NewLine + "Two Twelve Month:" + TwoTwelveMonth + Environment.NewLine + "Two Twelve Month Check:" + TwoTwelveMonthCheck + Environment.NewLine + "Family Single Month:" + FamilySingleMonth + Environment.NewLine + "Family Single Month Check:" + FamilySingleMonthCheck + Environment.NewLine + "Family Twelve Month:" + FamilyTwelveMonth + Environment.NewLine + "Family Twelve Month:" + FamilyTwelveMonthCheck;
         }
     }
 }
