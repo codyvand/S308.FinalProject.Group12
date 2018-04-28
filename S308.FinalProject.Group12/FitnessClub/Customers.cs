59656996efe71c76b74819ed1d17e0d42d8c94ee
zdobos <zdobos@indiana.edu>
2018-04-28 12:21:31 -0400

    Updated class scustomers

--- S308.FinalProject.Group12/FitnessClub/Customers.cs
+++ S308.FinalProject.Group12/FitnessClub/Customers.cs
@@ -8,6 +8,7 @@ namespace FitnessClub
 {
     class Customers
     {
+        public string PersonalFitnessGoal { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Weight { get; set; }
@@ -26,8 +27,9 @@ namespace FitnessClub
         {
 
         }
-        public Customers(string firstname, string lastname, string weight, string gender, string phone, string email, string age, string customertype, string startdate, string enddate, string monthlytrainingplan, string monthlylockerrental, string creditcardtype, string creditcardnumber)
+        public Customers(string personalfitnessgoal, string firstname, string lastname, string weight, string gender, string phone, string email, string age, string customertype, string startdate, string enddate, string monthlytrainingplan, string monthlylockerrental, string creditcardtype, string creditcardnumber)
         {
+            PersonalFitnessGoal = personalfitnessgoal;
             FirstName = firstname;
             LastName = lastname;
             Weight = weight;
@@ -45,7 +47,7 @@ namespace FitnessClub
         }
         public override string ToString()
         {
-            return "Customers" + Environment.NewLine + "First Name:" + FirstName + Environment.NewLine + "Last Name:" + LastName + Environment.NewLine + "Weight:" + Weight + Environment.NewLine + "Gender:" + Gender + Environment.NewLine + "Phone:" + Phone + Environment.NewLine + "Email:" + Email + Environment.NewLine + "Age:" + Age + Environment.NewLine + "Start Date:" + StartDate + Environment.NewLine + "End Date:" +EndDate + Environment.NewLine + "Monthly Training Plan:" + MonthlyTrainingPlan + Environment.NewLine +"Monthly Locker Rental:" + MonthlyLockerRental + Environment.NewLine + "Credit Card Type:" + CreditCardType +Environment.NewLine + "Credit Card Number:" + CreditCardNumber;
+            return "Customers" + Environment.NewLine +"Personal Fitness Goal:" + PersonalFitnessGoal + Environment.NewLine + "First Name:" + FirstName + Environment.NewLine + "Last Name:" + LastName + Environment.NewLine + "Weight:" + Weight + Environment.NewLine + "Gender:" + Gender + Environment.NewLine + "Phone:" + Phone + Environment.NewLine + "Email:" + Email + Environment.NewLine + "Age:" + Age + Environment.NewLine + "Start Date:" + StartDate + Environment.NewLine + "End Date:" +EndDate + Environment.NewLine + "Monthly Training Plan:" + MonthlyTrainingPlan + Environment.NewLine +"Monthly Locker Rental:" + MonthlyLockerRental + Environment.NewLine + "Credit Card Type:" + CreditCardType +Environment.NewLine + "Credit Card Number:" + CreditCardNumber;
         }
     }
 }
