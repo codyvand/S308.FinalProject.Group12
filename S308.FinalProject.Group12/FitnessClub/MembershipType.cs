8ece27bafdc85b4f59fc0cf2c5d7d58a632c621a
zdobos <zdobos@indiana.edu>
2018-04-29 13:50:50 -0400

    Added MembershipType

---
+++ S308.FinalProject.Group12/FitnessClub/MembershipType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessClub
+{
+    class MembershipType
+    {
+        public string MembershipName { get; set; }
+        public string MembershipPrice { get; set; }
+        public string MembershipLength { get; set; }
+        public string MembershipAvailibility { get; set; }
+
+        public MembershipType()
+        {
+
+        }
+        public MembershipType(string membershipname, string membershipprice, string membershiplength, string membershipavailibility)
+        {
+            MembershipName = membershipname;
+            MembershipPrice = membershipprice;
+            MembershipLength = membershiplength;
+            MembershipAvailibility = membershipavailibility;
+        }
+        public override string ToString()
+        {
+            return "MembershipType" + Environment.NewLine + "MembershipName:" + MembershipName + Environment.NewLine + "Membership Price:" + MembershipPrice + Environment.NewLine + "MembershipLength:" + MembershipLength + Environment;
+        }
+    }
+}
