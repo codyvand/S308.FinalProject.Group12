7db91de1cc387640e13076e0e06a7468965ea03f
zdobos <zdobos@indiana.edu>
2018-04-30 15:09:54 -0400

    Creating Membership PRice control

--- S308.FinalProject.Group12/FitnessClub/PriceControl.xaml.cs
+++ S308.FinalProject.Group12/FitnessClub/PriceControl.xaml.cs
@@ -21,16 +21,92 @@ namespace FitnessClub
     /// </summary>
     public partial class PriceControl : Window
     {
+        #region Properties & Constructor
         List<MembershipType> membershiptypeList;
         public PriceControl()
         {
-            //instantiate a list to hold the membership types
-            membershiptypeList = new List<MembershipType>();
-
-
-            //call the method to local the campus information and display
             InitializeComponent();
+            membershiptypeList = new List<MembershipType>();
+            dtgContact.ItemsSource = membershiptypeList;
         }
+        #endregion
+
+        #region Event Handlers
+        private void btnImport_Click_1(object sender, RoutedEventArgs e)
+        {
+            string strLine = "";
+            string strFilePath = @"..\..\..\Data\MembershipType.json";
+
+            try
+            {
+                membershiptypeList.Clear();
+                StreamReader reader = new StreamReader(strFilePath);
+
+                while (!reader.EndOfStream)
+                {
+                    strLine = reader.ReadLine();
+                    //MessageBox.Show(strLine);
+                    membershiptypeList.Add(ConvertToMembershipType(strLine));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in ready file: " + ex.Message);
+                return;
+            }
+
+            dtgContact.Items.Refresh();
+        }
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            MembershipType membershiptypeNew = new MembershipType(txtMembershipNameCreateData.Text.Trim(), txtMembershipCostCreateData.Text.Trim()
+                                        , txtMembershipAvailibilityCreateData.Text.Trim(), txtMembershipLengthCreateData.Text.Trim());
+
+            MessageBoxResult messageBoxResult = MessageBox.Show("Do you want to save the following contact?"
+                + Environment.NewLine + Environment.NewLine + membershiptypeNew.ToString()
+                , "Create New Contact"
+                , MessageBoxButton.YesNo);
+
+            if (messageBoxResult == MessageBoxResult.Yes)
+            {
+                membershiptypeList.Add(membershiptypeNew);
+                dtgContact.Items.Refresh();
+
+                AppendToFile(membershiptypeNew);
+
+                ClearScreen();
+                MessageBox.Show("New Contact Saved!");
+            }
+        }
+
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+            string strFilePath = @"..\..\..\Data\Contacts"
+    + ".json";
+            string strLine;
+
+            try
+            {
+                StreamWriter writer = new StreamWriter(strFilePath, false);
+                foreach (MembershipType c in membershiptypeList)
+                {
+                    strLine = String.Format("{0}|{1}|{2}|{3}", c.MembershipName
+                    , c.MembershipPrice, c.MembershipAvailibility, c.MembershipLength);
+                    writer.WriteLine(strLine);
+                }
+
+                writer.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in write file: " + ex.Message);
+                return;
+            }
+
+            MessageBox.Show("Export completed!" + Environment.NewLine + "File Created: " + strFilePath);
+        }
+        #endregion
+
         private void btnMainMenu_Click(object sender, RoutedEventArgs e)
         {
             Window winHomePage = new HomePage();
@@ -52,101 +128,6 @@ namespace FitnessClub
             this.Close();
         }
 
-        private MembershipType ConvertToMembershipType(string strLine)
-        {
-            string[] rawData;
-            rawData = strLine.Split(',');
 
-            MembershipType membershiptypeNew = new MembershipType(rawData[0], rawData[1], rawData[2]);
-            return membershiptypeNew;
-        }
-
-        private void btnClear_Click(object sender, RoutedEventArgs e)
-        {
-            //ClearScreen();
-            membershiptypeList.Clear();
-            dtgContact.Items.Refresh();
-        }
-
-
-        private void btnImport_Click(object sender, RoutedEventArgs e)
-        {
-            string strLine = "";
-            string strFilePath = @"..\..\..\Data\MembershipType.json";
-
-            try
-            {
-                membershiptypeList.Clear();
-                StreamReader reader = new StreamReader(strFilePath);
-
-                while (!reader.EndOfStream)
-                {
-                    strLine = reader.ReadLine();
-                    MessageBox.Show(strLine);
-                    membershiptypeList.Add(ConvertToMembershipType(strLine));
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error in ready file: " + ex.Message);
-                return;
-            }
-
-            dtgContact.Items.Refresh();
-
-        }
-
-        private void btnExport_Click_1(object sender, RoutedEventArgs e)
-        {
-            string strFilePath = @"..\..\..\Data\MembershipType"
-    + ".json";
-            string strLine;
-
-            try
-            {
-                StreamWriter writer = new StreamWriter(strFilePath, false);
-                foreach (MembershipType c in membershiptypeList)
-                {
-                    strLine = String.Format("{0},{1},{2}", c.MembershipName
-                    , c.MembershipPrice, c.MembershipAvailibility);
-                    writer.WriteLine(strLine);
-                }
-
-                writer.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error in write file: " + ex.Message);
-                return;
-            }
-
-            MessageBox.Show("Export completed!" + Environment.NewLine + "File Created: " + strFilePath);
-        }
-        private void AppendToFile(MembershipType membershiptypeNew)
-        {
-            string strFilePath = @"..\..\..\Data\MembershipType.txt";
-            string strLine;
-
-            try
-            {
-                StreamWriter writer = new StreamWriter(strFilePath, true);
-                strLine = String.Format("{0}|{1}|{2}", membershiptypeNew.MembershipName
-                    , membershiptypeNew.MembershipPrice, membershiptypeNew.MembershipAvailibility);
-                writer.WriteLine(strLine);
-                writer.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error in append file: " + ex.Message);
-                return;
-            }
-        }
-        private void ClearScreen()
-        {
-            txtMembershipNameCreateData.Text = "";
-            txtMembershipCostCreateData.Text = "";
-            txtMembershipAvailibilityCreateData.Text = "";
-
-        }
     }
 }
